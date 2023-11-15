fb82750c639c6cd17e2b33ba31a7f8c752f49af4
Alex Cook <alexandercook.eng@gmail.com>
2023-11-15 17:48:57 +0000

    Implemented __init__ for SHHSDataset_f, taking a list of nsrrids as input.

--- src/datasets/shhs_dataset_f.py
+++ src/datasets/shhs_dataset_f.py
@@ -1,10 +1,20 @@
 from pathlib import Path
 import pandas as pd
-import torch
-import torch.utils.data as d
+from datasets import BaseDataset_f
 
 
 # Custom dataset for handcrafted frequency features.
-class SHHSDataset_f(d.Dataset):
+class SHHSDataset_f(BaseDataset_f):
 
+    def __init__(self, nsrrids: list[int], resample=None):
+        data = []
+        root_path = Path(__file__).parent.parent.parent
+        for nsrrid in nsrrids:
+            participant_data = pd.read_csv(root_path / f"data/Processed/shhs/Frequency_Features/nsrrid_{nsrrid}.csv")
+            data.append(participant_data)
+        self.data = pd.concat(data, axis=0)
+
+        self.resample_factors = resample
+        if resample is not None:
+            self.resample()
 
