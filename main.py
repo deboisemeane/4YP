af1da4c40f0519bdcdab5e80794158b59b8d21c2
Alex Cook <alexandercook.eng@gmail.com>
2023-12-04 10:36:51 +0000

    Split size 500.

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Training the MLP on SHHS data
 
-split = {"train": 2000,
-         "val":   580,
-         "test":  290}
+split = {"train": 350,
+         "val":   100,
+         "test":  50}
 
 resample = {"2": 2.84}
 
@@ -23,9 +23,11 @@ data_config = SHHSConfig(split=split, data_type="f", resample=None)
 optimiser_config = AdamConfig(lr=0.0001)
 
 trainer = Train(data_config=data_config, optimiser_config=optimiser_config, model=MLP1, device=device)
-trainer.train(n_epochs=1, print_losses=True, weight_losses=True)
+trainer.train(n_epochs=50, print_losses=True, weight_losses=True)
 
-trainer.save_best_model()
+# Testing
+trainer.test()
+# plot_norm_confusion(trainer.confusion, 'MLP trained on SHHS-1 dataset (350-150-50 split)')
 
 # Plotting loss for training with SHHS
 fig, ax = plt.subplots()
@@ -34,8 +36,3 @@ labels = {"t": "Training",
           "v": "Validation"}
 trainer.plot_loss(ax=ax, labels=labels)
 plt.savefig(f'figures/weighted_loss_MLP1_shhs1_{split["train"]}-{split["val"]}-{split["test"]}.png')
-
-# Testing
-trainer.test()
-
-# plot_norm_confusion(trainer.confusion, 'MLP trained on SHHS-1 dataset (350-150-50 split)')
