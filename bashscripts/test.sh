22244b5651db44b12969a9351d06dfe063d2a4ea
Alex Cook <alexandercook.eng@gmail.com>
2023-12-07 11:31:37 +0000

    Check gpu availability on cluster

--- bashscripts/test.sh
+++ bashscripts/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH --time=10:0:00
+#SBATCH --time=1:00
 #SBATCH --output=outputs/test.stdout
+#SBATCH --gres=gpu:1
 
 source ~/miniconda/etc/profile.d/conda.sh
 conda run -n FYP python test.py
