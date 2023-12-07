79c5cdda886607b0e02212608dd426d336662240
Alex Cook <alexandercook.eng@gmail.com>
2023-12-07 11:31:37 +0000

    Check gpu availability on cluster

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from preprocess import SHHSPreprocessor
+import torch
+# pre = SHHSPreprocessor(art_rejection=True, lpf=False)
+# pre.process_t(incl_preceeding_epochs=2, incl_following_epochs=1)
 
-pre = SHHSPreprocessor(art_rejection=True, lpf=False)
-pre.process_t(incl_preceeding_epochs=2, incl_following_epochs=1)
-
+print(torch.cuda.is_available())
