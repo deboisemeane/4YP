27104e7942d06f29520d817c728656a467449054
deboisemeane <alexandercook2002@gmail.com>
2023-12-09 14:02:36 +0000

    Added Timer class.

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .calculate_ce_loss import calculate_ce_loss
 from .confusion_matrix import confusion_matrix
 from .accuracy_metrics import accuracy_metrics
 from .get_data_dir_shhs import get_data_dir_shhs
+from .timer import Timer
