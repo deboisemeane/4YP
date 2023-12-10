79da7c4ea238d990458d7389af0692ca625f2072
deboisemeane <alexandercook2002@gmail.com>
2023-12-10 00:25:11 +0000

    Timings and if __name__ == main to allow multiprocessing to work.

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
-import torch.cuda
-
-from scripts import Train, AdamConfig, SGDConfig, ISRUCConfig, SHHSConfig
-from utils import accuracy_metrics
-from visualisation import plot_norm_confusion
-from src.models import MLP1, MLP2
+import torch
+from scripts import Train, AdamConfig, SHHSConfig
+from src.models import MLP1
 import matplotlib.pyplot as plt
-
-# Find device
-print("Using cuda" if torch.cuda.is_available() else "Using cpu")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from utils import Timer
 
 
-# Training the MLP on SHHS data
+def main():
+    # Find device
+    #print("Using cuda" if torch.cuda.is_available() else "Using cpu")
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda")
 
-split = {"train": 1950,
-          "val":   557,
-          "test":  278}
+    # Training the MLP on SHHS data
+    #split = {"train": 350, "val": 100, "test": 50}
+    split = {"train": 1950, "val": 557, "test": 278}
+    resample = {"2": 2.84}
 
-resample = {"2": 2.84}
+    data_config = SHHSConfig(split=split, data_type="f", art_rejection=True, resample=None)
+    optimiser_config = AdamConfig(lr=0.0001)
 
-data_config = SHHSConfig(split=split, data_type="f", art_rejection=True, resample=None)
-optimiser_config = AdamConfig(lr=0.0001)
+    trainer = Train(data_config=data_config, optimiser_config=optimiser_config, model=MLP1, device=device)
 
-trainer = Train(data_config=data_config, optimiser_config=optimiser_config, model=MLP1, device=device)
+    timer = Timer()
+    timer.start()
+    trainer.train(n_epochs=50, print_losses=True, weight_losses=True)
+    time_train = timer.stop()
+    print(f"Total training time: {time_train}")
 
-trainer.train(n_epochs=50, print_losses=True, weight_losses=True)
+    # Testing
+    trainer.test()
 
-# Testing
-trainer.test()
-# plot_norm_confusion(trainer.confusion, 'MLP trained on SHHS-1 dataset (350-150-50 split)')
+    # Plotting loss for training with SHHS
+    fig, ax = plt.subplots()
+    ax.set_title("Weighted CrossEntropyLoss training 20-10-4 MLP with SHHS-1, Artefacts rejected")
+    labels = {"t": "Training", "v": "Validation"}
+    trainer.plot_loss(ax=ax, labels=labels)
+    plt.savefig(f'figures/art_rejection_weighted_cross_MLP1_shhs1_{split["train"]}-{split["val"]}-{split["test"]}.png')
 
-# Plotting loss for training with SHHS
-fig, ax = plt.subplots()
-ax.set_title("Weighted CrossEntropyLoss training 20-10-4 MLP with SHHS-1, Artefacts rejected")
-labels = {"t": "Training",
-          "v": "Validation"}
-trainer.plot_loss(ax=ax, labels=labels)
-plt.savefig(f'figures/art_rejection_weighted_cross_MLP1_shhs1_{split["train"]}-{split["val"]}-{split["test"]}.png')
+
+if __name__ == '__main__':
+    main()
