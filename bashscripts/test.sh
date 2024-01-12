a518a70fdffed4c5b88bc312d6274ffaab8a75b1
deboisemeane <alexandercook2002@gmail.com>
2024-01-12 17:32:13 +0000

    Fixed indentation.

--- bashscripts/test.sh
+++ bashscripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --output=outputs/test.stdout
-#SBATCH --gres=gpu:1
+##SBATCH --gres=gpu:1
 
 source ~/miniconda/etc/profile.d/conda.sh
 conda run -n FYP python test.py
