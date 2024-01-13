e1e0501e0ef3cc14ce41ac43079dcb3866b17162
Alex Cook <alexandercook.eng@gmail.com>
2024-01-13 12:04:16 +0000

    Added all processed shhs features to gitignore

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from preprocess import SHHSPreprocessor
 import torch
-pre = SHHSPreprocessor(art_rejection=True, lpf=False)
-pre.process_t(incl_preceeding_epochs=2, incl_following_epochs=1)
+#pre = SHHSPreprocessor(art_rejection=True, lpf=False)
+#pre.process_t(incl_preceeding_epochs=2, incl_following_epochs=1)
 
 pre = SHHSPreprocessor(art_rejection=True, lpf=True)
 pre.process_t(incl_preceeding_epochs=2, incl_following_epochs=1)
