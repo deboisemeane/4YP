c169c19960d4ec32962672e514704824045cbd09
Alex Cook <alexandercook.eng@gmail.com>
2024-01-14 16:47:37 +0000

    Added some total metrics and cohen's kappa

--- utils/accuracy_metrics.py
+++ utils/accuracy_metrics.py
@@ -1,36 +1,41 @@
 import numpy as np
 
 
-# Calculates accuracy metrics from a confusion matrix (rows: Actual, columns: Predicted)
+import numpy as np
+
 def accuracy_metrics(confusion_matrix):
-    FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)
-    FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)
-    TP = np.diag(confusion_matrix)
-    TN = confusion_matrix.sum() - (FP + FN + TP)
+    FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)  # False Positives
+    FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)  # False Negatives
+    TP = np.diag(confusion_matrix)  # True Positives
+    TN = confusion_matrix.sum() - (FP + FN + TP)  # True Negatives
 
-    # Sensitivity, hit rate, recall, or true positive rate
-    TPR = TP / (TP + FN)
-    # Specificity or true negative rate
-    TNR = TN / (TN + FP)
-    # Precision or positive predictive value
-    PPV = TP / (TP + FP)
-    # Negative predictive value
-    NPV = TN / (TN + FN)
-    # Fall out or false positive rate
-    FPR = FP / (FP + TN)
-    # False negative rate
-    FNR = FN / (TP + FN)
-    # False discovery rate
-    FDR = FP / (TP + FP)
+    # Class-specific metrics
+    TPR = TP / (TP + FN)  # True Positive Rate
+    TNR = TN / (TN + FP)  # True Negative Rate
+    PPV = TP / (TP + FP)  # Positive Predictive Value
+    NPV = TN / (TN + FN)  # Negative Predictive Value
+    FPR = FP / (FP + TN)  # False Positive Rate
+    FNR = FN / (TP + FN)  # False Negative Rate
+    FDR = FP / (TP + FP)  # False Discovery Rate
+    ACC = (TP + TN) / (TP + FP + FN + TN)  # Accuracy
 
-    # Overall accuracy
-    ACC = (TP + TN) / (TP + FP + FN + TN)
+    # Total metrics (weighted averages)
+    total_instances = confusion_matrix.sum()
+    weights_for_ppv = confusion_matrix.sum(axis=0) / total_instances  # Weights for PPV based on predicted class sizes
+    weights_for_npv = (confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)) / total_instances  # Weights for NPV based on negative predictions
+    weights_for_other_metrics = confusion_matrix.sum(axis=1) / total_instances  # Weights for other metrics based on actual class sizes
+    total_TPR = np.sum(TPR * weights_for_other_metrics)
+    total_PPV = np.sum(PPV * weights_for_ppv)
+    total_NPV = np.sum(NPV * weights_for_npv)
+    total_FDR = np.sum(FDR * weights_for_ppv)
+    total_ACC = np.sum(ACC * weights_for_other_metrics)
 
-    return {"TPR": TPR,
-            "TNR": TNR,
-            "PPV": PPV,
-            "NPV": NPV,
-            "FPR": FPR,
-            "FNR": FNR,
-            "FDR": FDR,
-            "ACC": ACC}
+    # Total Cohen's Kappa
+    Po = np.sum(TP) / total_instances
+    Pe = np.sum(confusion_matrix.sum(axis=0) * confusion_matrix.sum(axis=1)) / total_instances**2
+    total_Kappa = (Po - Pe) / (1 - Pe)
+
+    return {"TPR": TPR, "TNR": TNR, "PPV": PPV, "NPV": NPV, "FPR": FPR, "FNR": FNR, "FDR": FDR, "ACC": ACC,
+            "Total TPR": total_TPR, "Total PPV": total_PPV, "Total NPV": total_NPV, "Total FDR": total_FDR,
+            "Total ACC": total_ACC,
+            "Total Kappa": total_Kappa}
