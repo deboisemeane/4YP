4e9910d0d3d70ca198ccf35529ea9e5e9de1deb7
Alex Cook <alexandercook.eng@gmail.com>
2024-01-20 22:13:40 +0000

    Converted training losses to floats so that they can be plotted by matplotlib.

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def main():
     #device = torch.device("cuda")
 
     # Training the MLP on SHHS data
-    #split = {"train": 350, "val": 100, "test": 50}
+    split = {"train": 350, "val": 100, "test": 50}
     #split = {"train": 1950, "val": 557, "test": 278}
-    split = {"train": 1, "val": 1, "test": 1}
+    #split = {"train": 1, "val": 1, "test": 1}
     #resample = {"2": 2.84}
 
     data_config = SHHSConfig(split=split, data_type="t", art_rejection=True, lpf=False, resample=None)
@@ -24,7 +24,7 @@ def main():
 
     timer = Timer()
     timer.start()
-    trainer.train(n_epochs=2, print_losses=True, weight_losses=True)
+    trainer.train(n_epochs=1, print_losses=True, weight_losses=True)
     time_train = timer.stop()
     print(f"Total training time: {time_train}")
 
@@ -33,7 +33,7 @@ def main():
 
     # Plotting loss for training with SHHS
     fig, ax = plt.subplots()
-    ax.set_title("Weighted CrossEntropyLoss training CNN with SHHS-1, Artefacts rejected")
+    ax.set_title("Training CNN with LPF time-series EEG")
     labels = {"t": "Training", "v": "Validation"}
     trainer.plot_loss(ax=ax, labels=labels)
     plt.savefig(f'figures/art_rejection_weighted_cross_CNN_shhs1_{split["train"]}-{split["val"]}-{split["test"]}.png')
