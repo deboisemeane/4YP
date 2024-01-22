05edf789c2cf805188927ac08a46f9439ace1e11
Alex Cook <alexandercook.eng@gmail.com>
2024-01-22 14:17:35 +0000

    gpu

--- bashscripts/main.sh
+++ bashscripts/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=10:0:00
 #SBATCH --output=outputs/main.stdout
-##SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1
 
 source ~/miniconda/etc/profile.d/conda.sh
 conda run -n FYP python main.py
