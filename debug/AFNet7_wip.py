c3950cf70a7f7b0e39259194ff1a795c4bca3d9c
Alex Cook <alexandercook.eng@gmail.com>
2024-01-25 17:31:48 +0000

    Trying fewer layers.

--- debug/AFNet7_wip.py
+++ debug/AFNet7_wip.py
@@ -18,44 +18,37 @@ class AFNet_wip(nn.Module):
     #Initialises the various layers of the network
     def __init__(self):
         super().__init__()
-        padding7 = 3
-        padding5 = 2
-        padding3 = 1
-        self.conv1 = nn.Conv1d(1, 128, 7, 2, padding=padding7)
-        self.conv2 = nn.Conv1d(128, 128, 7, 2, padding=padding7)
-        self.conv3 = nn.Conv1d(128, 128, 7, 2, padding=padding7)
-        self.conv4 = nn.Conv1d(128, 128, 7, 2, padding=padding5)
-        self.conv5 = nn.Conv1d(128, 128, 7, 2, padding=padding5)
-        self.conv6 = nn.Conv1d(128, 128, 7, 2, padding=padding7)
-        self.conv7 = nn.Conv1d(128, 256, 7, 2, padding=padding7)
-        self.conv8 = nn.Conv1d(256, 256, 5, 2, padding=padding5)
-        self.conv9 = nn.Conv1d(256, 256, 5, 2, padding=padding5)
-        self.conv10 = nn.Conv1d(256, 256, 5, 2, padding=padding3)
-        self.conv11 = nn.Conv1d(256, 256, 3, 2, padding=padding3)
-        self.conv12 = nn.Conv1d(256, 256, 3, 2)
+        stride = 4
+        self.conv1 = nn.Conv1d(1, 16, 21, 2)
+        self.conv2 = nn.Conv1d(16, 32, 21, 2)
+        self.conv3 = nn.Conv1d(32, 32, 21, 2)
+        self.conv4 = nn.Conv1d(32, 64, 21, 2)
+        self.conv5 = nn.Conv1d(64, 64, 21, 2)
+        self.conv6 = nn.Conv1d(64, 128, 21, stride)
+        self.conv7 = nn.Conv1d(128, 128, 21, stride)
 
-        self.fc1 = nn.Linear(768, 100)
-        self.fc2 = nn.Linear(100, 4)
-        
-    #Defines the behaviour of a forward pass on an input
+        self.fc1 = nn.Linear(3712, 128)
+        self.fc2 = nn.Linear(128, 4)
+
+        # Defines the behaviour of a forward pass on an input
+
     def forward(self, x):
-        R = nn.ReLU()
-        #The convolutional layers
-        x = R(self.conv1(x))
-        x = R(self.conv2(x))
-        x = R(self.conv3(x))
-        x = R(self.conv4(x))
-        x = R(self.conv5(x))
-        x = R(self.conv6(x))
-        x = R(self.conv7(x))
-        x = R(self.conv8(x))
-        x = R(self.conv9(x))
-        x = R(self.conv10(x))
-        x = R(self.conv11(x))
-        x = R(self.conv12(x))
-        x = torch.flatten(x, start_dim=1)  # Start at dim1 such that the batch dimension is preserved.
-        x = R(self.fc1(x))
+        # Alex's zero padding to make 15000 into 18300
+        x = torch.nn.functional.pad(input=x, pad=(0, 3300), value=0)
+
+        # The convolutional layers
+        x = f.relu((self.conv1(x)))
+        x = (f.relu((self.conv2(x))))
+        x = (f.relu((self.conv3(x))))
+        x = (f.relu((self.conv4(x))))
+        x = (f.relu((self.conv5(x))))
+        x = (f.relu((self.conv6(x))))
+        x = f.relu((self.conv7(x)))
+
+        # Global max pooling and fully connected layer for classification
+        x = torch.flatten(x, start_dim=1)
+        x = f.relu(self.fc1(x))
         x = self.fc2(x)
+        # x = f.log_softmax(x, dim=1)
 
         return x
-
