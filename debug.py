64fc40412a5457de3dce817f09ee970e1bb750cb
Alex Cook <alexandercook.eng@gmail.com>
2024-01-31 22:36:40 +0000

    Split and n_epochs

--- debug.py
+++ debug.py
@@ -12,19 +12,19 @@ def main():
     #device = torch.device("cuda")
 
     # Training the MLP on SHHS data
-    split = {"train": 15, "val": 10, "test": 2}
-    #split = {"train": 1950, "val": 557, "test": 278}
+    #split = {"train": 15, "val": 10, "test": 2}
+    split = {"train": 1950, "val": 557, "test": 278}
     #split = {"train": 1, "val": 1, "test": 1}
     #resample = {"2": 2.84}
 
-    data_config = SHHSConfig(split=split, data_type="t", art_rejection=True, lpf=False, resample=None)
+    data_config = SHHSConfig(split=split, data_type="t", art_rejection=True, lpf=True, resample=None)
     optimiser_config = AdamConfig(lr=0.0001)
 
-    trainer = Train(data_config=data_config, optimiser_config=optimiser_config, model=Sors, device=device)
+    trainer = Train(data_config=data_config, optimiser_config=optimiser_config, model=AFNet, device=device)
 
     timer = Timer()
     timer.start()
-    trainer.train(n_epochs=5, print_losses=True, weight_losses=True)
+    trainer.train(n_epochs=12, print_losses=True, weight_losses=False)
     time_train = timer.stop()
     print(f"Total training time: {time_train}")
 
@@ -36,7 +36,7 @@ def main():
     ax.set_title("Training CNN with time-series EEG")
     labels = {"t": "Training", "v": "Validation"}
     trainer.plot_loss(ax=ax, labels=labels)
-    plt.savefig(f'figures/art_rejection_weighted_cross_CNN_shhs1_{split["train"]}-{split["val"]}-{split["test"]}.png')
+    plt.savefig(f'figures/art_rejection1_lpf1_lossweighting0_AFNet_shhs1_{split["train"]}-{split["val"]}-{split["test"]}.png')
 
 
 if __name__ == '__main__':
