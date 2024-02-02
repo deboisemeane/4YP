3cb212a506db781b010c605435392710cb8c8d68
Alex Cook <alexandercook.eng@gmail.com>
2024-02-02 23:29:42 +0000

    Flipped x-axis as well.

--- visualisation/plot_norm_confusion.py
+++ visualisation/plot_norm_confusion.py
@@ -8,7 +8,7 @@ def plot_norm_confusion(confusion, title):
     print(confusion)
     norm_confusion = confusion / (np.sum(confusion, axis=1).reshape(len(confusion), 1))
     norm_confusion = np.flip(norm_confusion, axis=0)    # Lionel prefers the confusion matrix figure to have a flipped y axis.
-
+    norm_confusion = np.flip(norm_confusion, axis=1)    # Lionel prefers the confusion matrix to have flipped x axis.
     fig, ax = plt.subplots()
     plt.imshow(norm_confusion)
 
@@ -16,7 +16,7 @@ def plot_norm_confusion(confusion, title):
 
     ax.set_xlabel('Predicted')
     ax.set_ylabel('Actual')
-    ax.set_xticks(np.arange(len(ticks)), ticks)
+    ax.set_xticks(np.arange(len(ticks)), ticks[::-1])  # Lionel prefers flipped x axis
     ax.set_yticks(np.arange(len(ticks)), ticks[::-1])  # Lionel prefers flipped y axis.
 
     # Loop over data dimensions and create text annotations.
@@ -30,8 +30,8 @@ def plot_norm_confusion(confusion, title):
 
 
 if __name__ == '__main__':
-    confusion = np.array([[3.7921e+04, 9.2200e+02 ,6.2000e+01, 1.4900e+02],
- [1.6902e+04, 8.5477e+04, 6.7440e+03, 1.4020e+04],
- [2.0100e+02, 1.2160e+03, 3.5449e+04, 5.8550e+03],
- [9.1600e+02, 1.1460e+03, 7.2800e+02, 7.3304e+04]])
+    confusion = np.array([[3.8220e+04, 1.8600e+03, 2.0000e+00, 2.6500e+02],
+ [1.4048e+04, 9.7393e+04, 3.4140e+03, 7.4130e+03],
+ [1.6800e+02, 3.3980e+03, 3.4827e+04, 5.0320e+03],
+ [4.8700e+02, 2.2990e+03, 2.9300e+02, 7.3662e+04]])
     plot_norm_confusion(confusion, "")
