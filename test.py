93aedac35ad60e53e4977617fdeb6cf61dfaa95e
Alex Cook <alexandercook.eng@gmail.com>
2024-02-03 23:00:01 +0000

    Preprocessing with no context

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 from preprocess import SHHSPreprocessor
 import torch
-pre = SHHSPreprocessor(art_rejection=True, lpf=False)
-pre.process_t(incl_preceeding_epochs=2, incl_following_epochs=1)
-
 pre = SHHSPreprocessor(art_rejection=True, lpf=True)
-pre.process_t(incl_preceeding_epochs=2, incl_following_epochs=1)
+pre.process_t(incl_preceeding_epochs=0, incl_following_epochs=0)
+
 
 #print(torch.cuda.is_available())
