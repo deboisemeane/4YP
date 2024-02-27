1306a326e52821eaceea301c0bf49f5e08f19bfc
deboisemeane <alexandercook2002@gmail.com>
2024-02-27 23:31:53 +0000

    Preprocessing rip and ecg with context (and a 1/4 RIP sample rate to give 3748 dimensionality).

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from preprocess import SHHSCardioPreprocessor
 import torch
 pre = SHHSCardioPreprocessor()
-pre.process(data_types=["ECG"])
+pre.process(data_types=["THOR RES"], incl_preceeding_epochs=2, incl_following_epochs=1)
+pre.process(data_types=["ECG"], incl_preceeding_epochs=2, incl_following_epochs=1)
 
 
