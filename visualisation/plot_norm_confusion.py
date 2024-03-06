8f78027e6d161b102ea9ae5796ade4ef10e3acb5
deboisemeane <alexandercook2002@gmail.com>
2024-03-06 16:42:20 +0000

    Improved text visibility and added colourmap options and colorbar.

--- visualisation/plot_norm_confusion.py
+++ visualisation/plot_norm_confusion.py
@@ -2,36 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Function to plot confusion matrix
-def plot_norm_confusion(confusion, title):
-    # Normalise the confusion by row
-    print(confusion)
+# Function to plot normalized confusion matrix
+def plot_norm_confusion(confusion, title, cmap='viridis'):
+    # Normalize the confusion by row
     norm_confusion = confusion / (np.sum(confusion, axis=1).reshape(len(confusion), 1))
-    norm_confusion = np.flip(norm_confusion, axis=0)    # Lionel prefers the confusion matrix figure to have a flipped y axis.
-    norm_confusion = np.flip(norm_confusion, axis=1)    # Lionel prefers the confusion matrix to have flipped x axis.
+    norm_confusion = np.flip(norm_confusion, axis=0)  # Flip y axis
+    norm_confusion = np.flip(norm_confusion, axis=1)  # Flip x axis
+
     fig, ax = plt.subplots()
-    plt.imshow(norm_confusion)
+    cax = ax.imshow(norm_confusion, cmap=cmap)
 
-    ticks = ['N3','N1/N2','REM','W']
+    # Add a color bar
+    plt.colorbar(cax)
 
+    ticks = ['N3', 'N1/N2', 'REM', 'W']
     ax.set_xlabel('Predicted')
     ax.set_ylabel('Actual')
-    ax.set_xticks(np.arange(len(ticks)), ticks[::-1])  # Lionel prefers flipped x axis
-    ax.set_yticks(np.arange(len(ticks)), ticks[::-1])  # Lionel prefers flipped y axis.
+    ax.set_xticks(np.arange(len(ticks)), ticks[::-1])
+    ax.set_yticks(np.arange(len(ticks)), ticks[::-1])
 
     # Loop over data dimensions and create text annotations.
     for i in range(len(ticks)):
         for j in range(len(ticks)):
+            color = 'white' if norm_confusion[i, j] < 0.5 else 'black'
             text = ax.text(j, i, np.round(norm_confusion[i, j], decimals=2),
-                           ha="center", va="center", color=str(1-norm_confusion[i, j]))
+                           ha="center", va="center", color=color)
 
     ax.set_title(title)
     plt.show()
 
 
 if __name__ == '__main__':
-    confusion = np.array([[3.8220e+04, 1.8600e+03, 2.0000e+00, 2.6500e+02],
- [1.4048e+04, 9.7393e+04, 3.4140e+03, 7.4130e+03],
- [1.6800e+02, 3.3980e+03, 3.4827e+04, 5.0320e+03],
- [4.8700e+02, 2.2990e+03, 2.9300e+02, 7.3662e+04]])
-    plot_norm_confusion(confusion, "")
+    confusion = np.array([[10844, 25921,   778,  2722],
+ [10006, 84583,  7725, 17311],
+ [  245, 11014, 27828,  2792],
+ [  583,  8381,  1302, 55663]])
+    plot_norm_confusion(confusion, "", cmap="plasma")
