3109e6acf832160407338746e7c5f69936474d60
Alex Cook <alexandercook.eng@gmail.com>
2024-03-23 21:33:51 +0000

    Processing ecg_rip with differing sample rates.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from preprocess import SHHSCardioPreprocessor
 import torch
 pre = SHHSCardioPreprocessor()
-pre.process(data_types=["ECG"], incl_preceeding_epochs=2, incl_following_epochs=1)
+pre.process(data_types=["ECG", "THOR RES"], incl_preceeding_epochs=2, incl_following_epochs=1)
 
 
