fd506df2405e86cfc0c9e1e4d8bec767c42b7c24
Alex Cook <alexandercook.eng@gmail.com>
2024-04-03 17:29:56 +0100

    Added models for classification with HR signal

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -6,3 +6,4 @@ from .Sors_nocontext2 import Sors_nocontext2
 from .Sors_cardiocnn import Sors_cardiocnn
 from .Sors_dualcnn import Sors_dualcnn
 from .Sors_largekernels import Sors_largekernels
+from .Sors_hr import Sors_hr
