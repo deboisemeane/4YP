0c260a07fab6121c6b16e6747a5126821a6c36c7
Alex Cook <alexandercook.eng@gmail.com>
2024-04-04 11:04:18 +0100

    Debugging no processed data found on cluster

--- debug.py
+++ debug.py
@@ -4,10 +4,12 @@ from src.models import MLP1, Sors
 import matplotlib.pyplot as plt
 from utils import Timer, get_data_dir_shhs
 from debug import AFNet, AFNet_wip
+from scripts.train import SHHSConfig
 
 
 def main():
     print(get_data_dir_shhs("rip_hr", True, True, 2, 1))
+    config = SHHSConfig(split = {"train": 1926, "val": 550, "test": 275}, data_type="rip_hr", art_rejection=True, prec_epochs=2, foll_epochs=1)
 
 
 if __name__ == '__main__':
