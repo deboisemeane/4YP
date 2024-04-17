df58d532c6e1c0725c18c6af7a8d512f56cce03f
Alex Cook <alexandercook.eng@gmail.com>
2024-04-17 20:56:51 +0100

    MLP1 CV

--- main5.py
+++ main5.py
@@ -1,7 +1,6 @@
 import torch
-from scripts import Train, AdamConfig, SHHSConfig
-from src.models import MLP1, Sors, Sors7, Sors_nocontext1, Sors_nocontext2, Sors_dualcnn, Sors_largekernels
-from debug import AFNet
+from scripts import Train, AdamConfig, SHHSConfig, KFoldConfig, KFold_CV
+from src.models import MLP1, Sors, Sors7, Sors_nocontext1, Sors_nocontext2, Sors_dualcnn, Sors_largekernels, Sors_rip_hr
 import matplotlib.pyplot as plt
 from utils import Timer
 
@@ -14,31 +13,26 @@ def main():
     # Training the MLP on SHHS data
     #split = {"train": 15, "val": 10, "test": 2}
     #split = {"train": 1950, "val": 557, "test": 278}
-    split = {"train": 1926, "val": 550, "test": 275}
+    #split = {"train": 1926, "val": 550, "test": 275}
     #split = {"train": 350, "val": 100, "test": 50}
     #resample = {"2": 2.84}
+    split = {"train": 0.7, "val": 0.2, "test": 0.1}
 
-    data_config = SHHSConfig(split=split, data_type="ecg", art_rejection=True, filtering=True, resample=None,
-                             prec_epochs=2, foll_epochs=1)
+    data_config = KFoldConfig(k=10, split=split, data_type="f", art_rejection=True, filtering=True, resample=None,
+                             prec_epochs=2, foll_epochs=1, equal_split="test")
     optimiser_config = AdamConfig(lr=0.0003)
 
-    trainer = Train(data_config=data_config, optimiser_config=optimiser_config, model=Sors, device=device)
-
+    #trainer = Train(data_config=data_config, optimiser_config=optimiser_config, model=Sors, device=device)
+    kfoldcv = KFold_CV(data_config, optimiser_config, device, MLP1)
     timer = Timer()
     timer.start()
-    trainer.train(n_epochs=12, print_losses=True, weight_losses=False)
+    kfoldcv.cross_validate(n_epochs=12, print_losses=True, weight_losses=True, weight_scalings=torch.tensor([1, 1, 1, 1]))
     time_train = timer.stop()
     print(f"Total training time: {time_train}")
 
-    # Testing
-    trainer.test()
-
     # Plotting loss for training with SHHS
-    fig, ax = plt.subplots()
-    ax.set_title("CardioExperiment3")
-    labels = {"t": "Training", "v": "Validation"}
-    trainer.plot_loss(ax=ax, labels=labels)
-    #plt.savefig(f'figures/cardio_sors_dualcnn_{split["train"]}-{split["val"]}-{split["test"]}.png')
+    kfoldcv.plot_loss("/figures/5fold_Sorsdualstream_rip_hr.png", "5-Fold Cross Validation of RIP HR Dual Stream Model")
+
 
 
 if __name__ == '__main__':
