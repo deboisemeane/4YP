501631ea060a2daae26d55408df5c12a5c19c4fd
Alex Cook <alexandercook.eng@gmail.com>
2024-04-18 11:21:48 +0100

    Added squeeze to remove unnecessary dims.

--- src/models/MLP.py
+++ src/models/MLP.py
@@ -13,6 +13,7 @@ class MLP1(nn.Module):   # Multi-Layer Perceptron with either 1 or 2 hidden laye
         initialiser(self.fc2.weight)
 
     def forward(self, x):
+        x = torch.squeeze(x)
         x = F.relu(self.fc1(x))
         # x = F.softmax(self.fc2(x))
         x = self.fc2(x)  # The nn.CrossEntropyLoss criterion internally computes softmax activation - we don't need it here.
